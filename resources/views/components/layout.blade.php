0aa8f04ca121ac442fd69e724a4d6db0ede4b160
hrasek <hosekmotor@seznam.cz>
2024-09-20 09:59:10 +0200

    feat: Added boxes for future jobs filtering.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel Job Board</title>
-    @vite('resources/css/app.css')
+     @vite('resources/css/app.css')
 </head>
 <body class="mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
 {{ $slot }}
