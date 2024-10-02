46deda8028e4319b6c0b777289fb64616b375feb
hrasek <hosekmotor@seznam.cz>
2024-10-02 11:49:59 +0200

    feat: Added logout logic.

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,33 @@
      @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-300 text-slate-700">
-{{ auth()->user()->name ?? 'Guest' }}
+<nav class="mb-8 flex justify-between text-lg font-medium">
+    <ul class="flex space-x-2">
+        <li>
+            <a href="{{ route('jobs.index') }}">Home</a>
+        </li>
+    </ul>
+    <ul class="flex space-x-2">
+        @auth
+            <li>
+                {{ auth()->user()->name ?? 'Anonymous' }}
+            </li>
+            <li>
+                <form action="{{ route('auth.destroy') }}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button>Logout</button>
+                </form>
+            </li>
+        @else
+            <li>
+                <a href="{{ route('auth.create') }}">Sign in</a>
+            </li>
+        @endauth
+
+    </ul>
+</nav>
 {{ $slot }}
 </body>
+
 </html>
