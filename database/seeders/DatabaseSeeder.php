6b5ed56080b911c582079909d0432d7862c3a42e
hrasek <hosekmotor@seznam.cz>
2024-10-07 11:55:59 +0200

    feat: Added JobApplication model and neccesary relations.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\Employer;
 use App\Models\Job;
+use App\Models\JobApplication;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -37,7 +38,18 @@ class DatabaseSeeder extends Seeder
             ]);
         }
 
-        // User::factory(10)->create();
+        foreach ($users as $user) {
+            $jobs = Job::inRandomOrder()->take(rand(0,4))->get();
+
+            foreach ($jobs as $job) {
+                JobApplication::factory()->create(
+                    [
+                        'job_id' => $job->id,
+                        'user_id' => $user->id,
+                    ]
+                );
+            }
+        }
 
 
     }
