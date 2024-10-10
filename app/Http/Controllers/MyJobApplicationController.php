6308ee1b84cfd83ed0e37032981ec32106a40708
hrasek <hosekmotor@seznam.cz>
2024-10-10 11:24:35 +0200

    feat: Added my applications Controller and View.

---
+++ app/Http/Controllers/MyJobApplicationController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyJobApplicationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('my_job_application.index',
+        [
+            'applications' => auth()->user()->jobApplications()
+                ->with('job', 'job.employer')
+                ->latest()->get()
+        ]
+        );
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
+}
