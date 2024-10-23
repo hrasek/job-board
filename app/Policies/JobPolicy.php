3dbc3b946e3d3dea2d137fd6b8bcf41c6cece705
Hrasek <hosekmotor@seznam.cz>
2024-10-23 10:51:32 +0200

    feat: Updated job policy.

--- app/Policies/JobPolicy.php
+++ app/Policies/JobPolicy.php
@@ -15,6 +15,10 @@ class JobPolicy
     {
         return True;
     }
+    public function viewAnyEmployer(User $user): bool
+    {
+        return True;
+    }
 
     /**
      * Determine whether the user can view the model.
@@ -29,15 +33,23 @@ class JobPolicy
      */
     public function create(User $user): bool
     {
-        return False;
+        return $user->employer !== null;
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Job $job): bool
+    public function update(User $user, Job $job): bool|Response
     {
-        return False;
+        if ($job->employer->user_id !== $user->id)
+        {
+            return false;
+        }
+
+        if ($job->jobApplications()->count() > 0){
+            return Response::deny('Cannot change the job with applications');
+        }
+        return true;
     }
 
     /**
@@ -45,7 +57,7 @@ class JobPolicy
      */
     public function delete(User $user, Job $job): bool
     {
-        return False;
+        return $job->employer->user_id == $user->id;
     }
 
     /**
@@ -53,16 +65,14 @@ class JobPolicy
      */
     public function restore(User $user, Job $job): bool
     {
-        return False;
-    }
+        return $job->employer->user_id == $user->id;    }
 
     /**
      * Determine whether the user can permanently delete the model.
      */
     public function forceDelete(User $user, Job $job): bool
     {
-        return False;
-    }
+        return $job->employer->user_id == $user->id;    }
 
     public function apply(User $user, Job $job): bool
     {
